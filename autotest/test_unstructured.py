11429f3b8166193cafbe73276f78ad9c239dc1cd
Joseph Hughes <jdhughes99+1@gmail.com>
2025-10-31 16:48:13 -0500

    fix lint issues

--- autotest/test_unstructured.py
+++ autotest/test_unstructured.py
@@ -294,7 +294,6 @@ def test_unstructured():
     ws.mkdir(parents=True, exist_ok=True)
     sim, pm_fname = build_model(ws, name=name)
     solve_adjoint(ws, pm_fname)
-    disv_riv, disv_drn = get_observations(sim)
 
     result_disv = get_sensitivities(ws)
 
@@ -303,7 +302,6 @@ def test_unstructured():
     ws.mkdir(parents=True, exist_ok=True)
     _, pm_fname = build_model(ws, name=name)
     solve_adjoint(ws, pm_fname)
-    disu_riv, disu_drn = get_observations(sim)
 
     result_disu = get_sensitivities(ws)
 

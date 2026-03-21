f18d508ccf46bc2dea7b9ae313de2c33879b537a
Joseph Hughes <jdhughes99+1@gmail.com>
2026-03-21 15:19:06 -0500

    doc: add rendered notebooks to readthedocs (#32)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import sys
 from datetime import datetime
 from pathlib import Path
@@ -7,6 +8,9 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parents[2]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
+
+# -- determine if running on readthedocs ------------------------------------
+on_rtd = os.environ.get("READTHEDOCS") == "True"
 
 
 def _read_version() -> str:
@@ -30,7 +34,20 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "myst_parser",
+    "nbsphinx",
 ]
+
+# Settings for GitHub actions integration
+if on_rtd:
+    extensions.append("rtds_action")
+    rtds_action_github_repo = "INTERA-Inc/mf6adj"
+    # This will overwrite the .docs/Notebooks directory
+    # with the notebooks downloaded & extracted from CI
+    # artifacts, which is fine. We want to render those
+    # with output, not clean ones from version control.
+    rtds_action_path = "examples"
+    rtds_action_artifact_prefix = "notebooks-for-"
+    rtds_action_github_token = os.environ.get("GITHUB_TOKEN", None)
 
 autosummary_generate = True
 
@@ -59,3 +76,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+
+nbsphinx_execute = "never"

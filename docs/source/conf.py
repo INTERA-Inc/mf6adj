f7a7ed5ce222deacc88f9c8559d9e26008b5d72b
Joseph Hughes <jdhughes99+1@gmail.com>
2026-03-22 12:19:27 -0500

    doc: fix paths for uploaded assets (#36)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,13 +41,13 @@ extensions = [
 if on_rtd:
     extensions.append("rtds_action")
     rtds_action_github_repo = "INTERA-Inc/mf6adj"
-    # This will overwrite the .docs/Notebooks directory
+    # This will overwrite the docs/Notebooks directory
     # with the notebooks downloaded & extracted from CI
     # artifacts, which is fine. We want to render those
     # with output, not clean ones from version control.
     rtds_action_path = "examples"
-    rtds_action_artifact_prefix = "notebooks-for-"
-    rtds_action_github_token = os.environ.get("RTDS_GITHUB_TOKEN", None)
+    rtds_action_artifact_prefix = "rendered-notebooks"
+    rtds_action_github_token = os.environ.get("GITHUB_TOKEN", None)
 
 autosummary_generate = True
 

423eb26e4020163488e1e086266a148c7276a56f
Joseph Hughes <jdhughes99+1@gmail.com>
2026-03-30 12:50:21 -0500

    release: resync develop with main (#54)

--- mf6adj/version.py
+++ mf6adj/version.py
@@ -1,3 +1,4 @@
-"""Package version information for `mf6adj`."""
+# mf6adj version file automatically created using
+# update_version.py on March 29, 2026 15:12:36
 
-__version__ = "1.0.0.dev0"
+__version__ = "1.0.0"
